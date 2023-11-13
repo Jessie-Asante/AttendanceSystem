594b80545c92367dafb8320e6eb52d66415e9601
osman.adamu <uthmanadam87@gmail.com>
2023-11-13 11:03:41 +0000

    hide

--- Innorik.Attendance.System.Application/Command/Query/Handlers/SearchAttendanceCommandHandler.cs
+++ Innorik.Attendance.System.Application/Command/Query/Handlers/SearchAttendanceCommandHandler.cs
@@ -1,12 +1,5 @@
-﻿using AutoMapper;
-using Innorik.Attendance.System.Application.Command.Query.Request;
+﻿using Innorik.Attendance.System.Application.Command.Query.Request;
 using Innorik.Attendance.System.Infrastructure.Persistence;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static Innorik.Attendance.System.Application.Dtos.QueryDto;
 
 namespace Innorik.Attendance.System.Application.Command.Query.Handlers
