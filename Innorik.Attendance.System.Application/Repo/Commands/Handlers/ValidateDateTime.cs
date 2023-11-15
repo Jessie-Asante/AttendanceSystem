2d6f9ff712f926f0fdd7600240a6acdf72a9bcfc
osman.adamu <uthmanadam87@gmail.com>
2023-11-15 14:05:08 +0000

    all

---
+++ Innorik.Attendance.System.Application/Repo/Commands/Handlers/ValidateDateTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innorik.Attendance.System.Application.Command.Commands.Handlers
+{
+    public class ValidateDateTime
+    {
+        public ApiResponse ValidDate(DateTime currentDate)
+        {
+
+            var serverDate = DateTime.UtcNow;
+            bool isCompare = currentDate > serverDate || currentDate < serverDate;
+            if (isCompare == true)
+            {
+                var response = new ApiResponse()
+                {
+                    IsSuccessful = true,
+                    Message = $"You are late"
+                };
+                return response;
+            }
+            var result = new ApiResponse()
+            {
+                IsSuccessful = true,
+                Message = $"You are on time"
+            };
+            return result;
+        }
+    }
+}
---
+++ Innorik.Attendance.System.Application/Repo/Query/Request/GetTodaysLoginsRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Innorik.Attendance.System.Application.Dtos.QueryDto;
+
+namespace Innorik.Attendance.System.Application.Command.Query.Request
+{
+    public class GetTodaysLoginsRequest : IRequest<IReadOnlyList<GetAllCheckInDto>>
+    {
+        public string? SearchText { get; set; }
+    }
+}
